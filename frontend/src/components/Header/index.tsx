302532c4aecfbbb67fa2926289f3dbe6da0e0e37
Edvan Junior <edvanbrito.j@hotmail.com>
2022-07-12 15:07:24 -0300

    Header component and SalesCard component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import logo from "../../assets/img/logo-header.svg";
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/EdvanLSBJunior"> Edvan Lázaro</a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
